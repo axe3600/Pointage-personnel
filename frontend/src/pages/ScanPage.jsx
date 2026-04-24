fe2e2f77f76c21669bd0633b64b2c4688342c18a
axe3600 <axelemmanuel360@gmail.com>
2026-04-24 12:04:35 +0000

    Message clair décrivant la modification

--- frontend/src/pages/ScanPage.jsx
+++ frontend/src/pages/ScanPage.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
+import { useEffect } from "react"
+import { useSearchParams } from "react-router-dom"
 
 function ScanPage() {
 
-  const [name, setName] = useState("")
-  const [message, setMessage] = useState("")
+  const [params] = useSearchParams()
 
-  const handleScan = (type) => {
+  const name = params.get("name")
+  const type = params.get("type")
 
-    if (!name) return alert("Entre ton nom")
+  useEffect(() => {
+
+    if (!name || !type) return
 
     fetch("http://localhost:5000/api/pointages/scan", {
       method: "POST",
@@ -19,66 +22,25 @@ function ScanPage() {
     .then(res => res.json())
     .then(data => {
 
-      // 🔥 Sauvegarde local
+      // 🔥 SAUVEGARDE LOCAL
       const saved = JSON.parse(localStorage.getItem("pointages")) || []
       saved.push(data)
       localStorage.setItem("pointages", JSON.stringify(saved))
 
-      setMessage(
+      alert(
         type === "arrival"
-          ? `✅ Arrivée enregistrée à ${data.arrival}`
-          : `✅ Départ enregistré à ${data.departure}`
+          ? `Arrivée enregistrée à ${data.arrival}`
+          : `Départ enregistré ;à ${data.departure}`
       )
     })
-  }
+
+  }, [])
 
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-50">
-
-      <div className="bg-white p-6 rounded-2xl shadow w-[320px]">
-
-        <h2 className="text-lg font-semibold mb-4 text-center">
-          Pointage rapide
-        </h2>
-
-        {/* NOM */}
-        <input
-          type="text"
-          placeholder="Ton nom"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full p-2 border rounded mb-4"
-        />
-
-        {/* BOUTONS */}
-        <div className="flex gap-2 mb-3">
-
-          <button
-            onClick={() => handleScan("arrival")}
-            className="flex-1 bg-green-500 text-white p-2 rounded"
-          >
-            Arrivée
-          </button>
-
-          <button
-            onClick={() => handleScan("departure")}
-            className="flex-1 bg-black text-white p-2 rounded"
-          >
-            Départ
-          </button>
-
-        </div>
-
-        {/* MESSAGE */}
-        {message && (
-          <p className="text-center text-sm text-green-600">
-            {message}
-          </p>
-        )}
-
-      </div>
+    <div className="h-screen flex items-center justify-center">
+      <h2 className="text-xl">Scan en cours...</h2>
     </div>
   )
 }
 
-export default ScanPage;
+export default ScanPage
